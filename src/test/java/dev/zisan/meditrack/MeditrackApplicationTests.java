9f24cc06d31fa9bd012ea304e7b62d12315ee95e
abdulazizizisan <binabdulaziz4983@gmail.com>
2026-04-11 22:49:38 +0600

    configure application properties and dependencies for development and testing environments

--- src/test/java/dev/zisan/meditrack/MeditrackApplicationTests.java
+++ src/test/java/dev/zisan/meditrack/MeditrackApplicationTests.java
@@ -2,8 +2,10 @@ package dev.zisan.meditrack;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MeditrackApplicationTests {
 
 	@Test
